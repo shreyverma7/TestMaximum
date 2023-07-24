c7b3612d064234c1f7bb9495d7da977a8ef03cb0
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-24 15:17:27 +0530

    [Shrey] Add : Maximum of three interger

---
+++ TestMaximum/Operation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class Operation
+    {
+        public void FindMaxInteger(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+                Console.WriteLine("a in max");
+            if (b >= a && b >= c)
+                Console.WriteLine("b in max");
+            if (c >= b && c >= a)
+                Console.WriteLine("c in max");
+        }
+    }
+}
--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using TestMaximum;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Genreric Method & Classes");
+        Console.WriteLine("Finding the maximum of three");
+        Operation operation = new Operation();
+        operation.FindMaxInteger(1, 2, 3);
     }
 }
