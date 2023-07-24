8f9588f0ec031e3d51c4601005c7e9c047844286
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-24 15:42:10 +0530

    [Shrey] Add : Adding multiple generic

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -8,6 +8,10 @@ internal class Program
         Console.WriteLine("Finding the maximum of three");
         GenericClass<int> operation = new GenericClass<int>();
         operation.FindMax(1, 2, 3);
-        
+        GenericClass<float> operation1 = new GenericClass<float>();
+        operation1.FindMax(2.5f, 9.9f, 11.2f);
+        GenericClass<string> operation2 = new GenericClass<string>();
+        operation2.FindMax("Shrey", "is", "good");
+
     }
 }
