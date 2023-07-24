ee17d6c31aab927a5c0627d79b023b7219f06117
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-24 15:33:57 +0530

    [Shrey] Add : Adding generic class and method

---
+++ TestMaximum/GenericClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class GenericClass<T> where T : IComparable
+    
+    {
+        public void FindMax(T a, T b, T c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+                Console.WriteLine("A is Maximum");
+            if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+                Console.WriteLine("B is Maximum");
+            if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
+                Console.WriteLine("C is Maximum");
+        }
+           
+        
+    }
+}
--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -4,11 +4,10 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Genreric Method & Classes");
+        
         Console.WriteLine("Finding the maximum of three");
-        Operation operation = new Operation();
-        operation.FindMaxInteger(1, 2, 3);
-        operation.FindMaxFloat(1.2f, 2.34f, 3.23f);
-        operation.FindMaxString("First", "Second", "Third");
+        GenericClass<int> operation = new GenericClass<int>();
+        operation.FindMax(1, 2, 3);
+        
     }
 }
